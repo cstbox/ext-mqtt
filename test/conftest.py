f11b842a30dcc7df6657eeca2520aef145865c03
Eric Pascual <eric.pascual@cstb.fr>
2015-07-31 11:33:12 +0200

    added shared fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,7 @@
 
 __author__ = 'Eric Pascual - CSTB (eric.pascual@cstb.fr)'
 
+import os
 import pytest
 
 
@@ -12,3 +13,18 @@ def pytest_addoption(parser):
 @pytest.fixture
 def show_log(request):
     return request.config.getoption("--show-log")
+
+
+def pytest_runtest_setup(item):
+    print("----------- setting up %s (kwd=%s)" % (item, [kwd for kwd in item.keywords]))
+
+
+@pytest.fixture
+def cfg():
+    import json
+    with file(os.path.join(os.path.dirname(__file__), 'fixtures/gateway.cfg')) as fp:
+        cfg_dict = json.load(fp)
+    assert cfg_dict
+    return cfg_dict
+
+
