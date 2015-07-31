b10b445675fb28322ae4be565ba0a9d25e07d264
Eric Pascual <eric.pascual@cstb.fr>
2015-07-31 11:31:41 +0200

    configuration revisited

--- lib/python/pycstbox/mqtt/dbus_binding.py
+++ lib/python/pycstbox/mqtt/dbus_binding.py
@@ -35,12 +35,14 @@ import time
 import threading
 
 import dbus.service
+from paho.mqtt import client as mqtt_client
 
 from pycstbox import dbuslib
 from pycstbox import evtmgr
 from pycstbox.log import Loggable
-from .core import MQTTConnector, InboundAdapter, OutboundAdapter
-from .config import *
+
+from .core import ConfigurableGatewayMixin
+from .errors import MQTTGatewayError
 
 SERVICE_NAME = "MQTTGateway"
 
@@ -49,13 +51,12 @@ OBJECT_PATH = "/service"
 SERVICE_INTERFACE = dbuslib.make_interface_name(SERVICE_NAME)
 
 
-class MQTTGatewayServiceObject(Loggable, dbus.service.Object):
+class MQTTGatewayServiceObject(dbus.service.Object, ConfigurableGatewayMixin, Loggable):
     """ The service object wrapping the gateway process.
 
     It takes care of all the generic tasks. It :
 
-        - listens to both sensor and control CSTBox buses for processing circulating messages for
-          forwarding them in the MQTT world
+        - listens to CSTBox buses for processing circulating messages for forwarding them in the MQTT world
         - creates a MQTT client for the broker, and listens to the relevant MQTT topics,
           for injecting the corresponding messages in the CSTBox internal communication bus
 
@@ -69,13 +70,11 @@ class MQTTGatewayServiceObject(Loggable, dbus.service.Object):
     It is valid to omit one of them, the absence of an adapter meaning that nothing will go through in the
     corresponding direction.
     """
-    DEFAULT_BROKER_PORT = 61613     #: default MQTT broker connection port
-
     _mqttc = None
     _cbx_publishers = {}
     _lock = None
     _inbound_adapter = None
-    _outbound_adapter = None
+    _outbound_adapters = {}
 
     def __init__(self, cfg):
         """
@@ -99,92 +98,13 @@ class MQTTGatewayServiceObject(Loggable, dbus.service.Object):
     def configure(self, cfg):
         """ Configures the service object.
 
-        The configuration data are provided as a dictionary, which keys and structure are :
-
-            - `broker` : sub-dictionary providing the broker connection information
-
-                - `host` : public host name or IP of the server
-                - `port` : connection port, defaulted to :py:attr:`DEFAULT_BROKER_PORT` if not
-                  provided
-                - `client_id` : optional identifier attached to the MQTT client
-
-            - `auth` : optional sub-dictionary containing the authentication information
-
-                - `username` : login
-                - `password` : guess what...
-
-            - `listened_topics` : optional list of MQTT topics or topic patterns the gateway must subscribe
+        Consists in configuring the gateway adapters and the MQTT connector, which is then
+        connected to its event handler.
 
         :param dict cfg: configuration data as a dictionary
         """
-        try:
-            # configure the adapters, passing them their specific sub-dictionary
-            cfg_adapters = cfg[CFG_ADAPTERS]
-            if not cfg_adapters:
-                raise ConfigurationError('at least one of input or output adapter must be configured')
-
-            self._inbound_adapter = self._configure_adapter(cfg_adapters.get(CFG_INBOUND, None))
-            self._outbound_adapter = self._configure_adapter(cfg_adapters.get(CFG_OUTBOUND, None))
-
-        except ValueError as e:
-            raise ConfigurationError(e)
-
-        except ConfigurationError:
-            raise
-
-        else:
-            self._mqttc = self._configure_mqtt_connector(cfg)
-
-    def _configure_mqtt_connector(self, cfg):
-        try:
-            cfg_broker = cfg[CFG_BROKER]
-            broker_host = cfg_broker[CFG_HOST]
-            broker_port = cfg_broker.get(CFG_PORT, self.DEFAULT_BROKER_PORT)
-            client_id = cfg_broker.get(CFG_CLIENT_ID, self.DEFAULT_BROKER_PORT)
-
-            cfg_auth = cfg.get(CFG_AUTH, None)
-            if cfg_auth:
-                login = cfg_auth[CFG_USER]
-                password = cfg_auth[CFG_PASSWORD]
-            else:
-                login = password = None
-
-            # TODO handle TLS parms et al.
-
-        except KeyError:
-            raise ValueError('invalid configuration data')
-
-        else:
-            topics = self._inbound_adapter.get_listened_topics() if self._inbound_adapter else None
-            mqttc = MQTTConnector(
-                broker=broker_host,
-                port=broker_port,
-                client_id=client_id,
-                login=login,
-                password=password,
-                topics=topics,
-                logger=self
-            )
-
-            mqttc.on_message = self._on_message
-            return mqttc
-
-    def _configure_adapter(self, cfg):
-        """ Creates and configures an adapter based on the configuration data.
-
-        Does nothing and returns None if the configuration contains no data or is None.
-
-        :param dict cfg: the inbound adapter configuration data
-        :return: the created adapter (or None)
-        :rtype: :py:class:`InboundAdapter` or :py:class:`OutboundAdapter`
-        """
-        if not cfg:
-            return None
-
-        klass = symbol_for_name(cfg[CFG_ADAPTER_CLASS])
-        adapter = klass()
-        adapter.configure(cfg)
-        return adapter
+        self._mqttc, self._inbound_adapter, self._outbound_adapters = \
+            ConfigurableGatewayMixin.configure(self, cfg)
 
     def _on_message(self, client, user_data, message):
         if self._inbound_adapter:
@@ -200,14 +120,20 @@ class MQTTGatewayServiceObject(Loggable, dbus.service.Object):
     @staticmethod
     def _channel_event_handler(channel):
         def _event_handler(self, timestamp, var_type, var_name, data):
-            if self._outbound_adapter:
+            try:
+                adapter = self._outbound_adapters[channel]
+            except KeyError:
+                pass
+            else:
                 with self._lock:
-                    mqtt_message = self._outbound_adapter.handle_event(timestamp, channel, var_type, var_name, data)
+                    mqtt_message = adapter.handle_event(timestamp, var_type, var_name, data)
                     if mqtt_message:
                         self.log_debug(
                             "forwarded to MQTT: timestamp=%s channel=%s var_type=%s var_name=%s data=%s",
                             timestamp, channel, var_type, var_name, data)
-                        self._mqttc.publish(*mqtt_message)
+                        result, mid = self._mqttc.publish(*mqtt_message)
+                        if result != mqtt_client.MQTT_ERR_SUCCESS:
+                            raise MQTTGatewayError("publish failed (rc=%d)" % result)
 
         return _event_handler
 
@@ -216,28 +142,24 @@ class MQTTGatewayServiceObject(Loggable, dbus.service.Object):
         """
         self.log_info('starting...')
 
-        # collects first all the event manager service objects we need before
-        # connecting to anything
-        # Note: current implementation handles sensor events only, but provisions are made for extending this
-        # in the future
-        svcs = []
-        for channel in [evtmgr.SENSOR_EVENT_CHANNEL]:
+        # connect to CSTBox event channels
+        for channel, adapter in self._outbound_adapters.iteritems():
             try:
                 svc = evtmgr.get_object(channel)
             except dbus.exceptions.DBusException as e:
-                self.log_error("cannot connect to event channel : %s (%s)", channel, e)
-                raise MQTTGatewayError()
+                msg = "cannot connect to event channel : %s (%s)" % (channel, e)
+                self.log_error(msg)
+                raise MQTTGatewayError(msg)
             else:
-                svcs.append((channel, svc))
+                svc.connect_to_signal("onCSTBoxEvent",
+                                      self._channel_event_handler(channel),
+                                      dbus_interface=evtmgr.SERVICE_INTERFACE)
+                self.log_info('connected to event channel : %s', channel)
 
-        # everybody is here, so we can start to work
-        for channel, svc in svcs:
-            svc.connect_to_signal("onCSTBoxEvent",
-                                  self._channel_event_handler(channel),
-                                  dbus_interface=evtmgr.SERVICE_INTERFACE)
-            self.log_info('connected to event channel : %s', channel)
-
+        # connect MQTT message handler and start the client
+        self._mqttc.on_message = self._on_message
         self._mqttc.run()
+
         self.log_info('started.')
 
     def stop(self):
